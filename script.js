19f7fe6f79cbd77327896e73af7950707e470fcd
AlexsonSilva <21643@student.dorset-college.ie>
2021-01-25 21:47:27 +0000

    commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function myAlphabetLength(){
+    console.log(myAlphabetLength.length);
+}
+
+const Planets = ['Mercury', 'Earth', 'Mars', 'Venus', 'Jupter'];
+console.log(Planets[0]);
+console.log(Planets[1]);
+console.log(Planets[2]);
+console.log(Planets[3]);
+console.log(Planets[4]);
+
+const wowDatatypes = [5], [Hi], [];
+console.log(wowDatatypes[0]);
+console.log(wowDatatypes[1]);
+console.log(wowDatatypes[2]);
